4fd6b009856291ba305cb6a8934fb77769ff5d54
Prajwal Bharti <prajwalbharti7777@gmail.com>
2025-01-06 16:40:48 +0530

    Spring Data Rest

---
+++ 07-spring-boot-rest-crud-employee-Data-Rest/src/main/java/com/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.cruddemo.dao;
+
+import com.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository <Employee,Integer> {
+}
---
+++ 07-spring-boot-rest-crud-employee-Data-Rest/src/main/java/com/cruddemo/entity/Employee.java
@@ -0,0 +1,70 @@
+package com.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="first_name")
+    private String firstname;
+    @Column(name="last_name")
+    private String lastname;
+    @Column(name="email")
+    private String email;
+
+    Employee(){
+
+    }
+
+    public Employee(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
