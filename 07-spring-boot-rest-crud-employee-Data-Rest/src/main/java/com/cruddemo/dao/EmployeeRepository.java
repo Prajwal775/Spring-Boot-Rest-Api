ec455b7b24436ddcf3c1e31bab0cd0267965d04e
Prajwal Bharti <prajwalbharti7777@gmail.com>
2025-01-06 16:03:01 +0530

    Remove folder 07-spring-boot-rest-crud-employee-Data-Rest

--- 07-spring-boot-rest-crud-employee-Data-Rest/src/main/java/com/cruddemo/dao/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cruddemo.dao;
-
-import com.cruddemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository <Employee,Integer> {
-}
